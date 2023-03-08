125c134f1ebd1b4481e44d455ed1d737fab96771
mukeshdreddy <mukeshdreddy@gmail.com>
2023-03-08 15:20:41 +1100

    Update_Terraform_alb

--- main.tf
+++ main.tf
@@ -40,8 +40,7 @@ data "aws_subnet" "reuse-2" {
   }
 }
 
-
-##########Terraform Imported Target Group######
+##########Terraform Imported Target Group - 1######
 resource "aws_lb_target_group" "Wildcard-Prod-apac-f4f-com" {
   name = "Wildcard-Prod-apac-f4f-com"
   target_type = "instance"
@@ -54,12 +53,51 @@ resource "aws_lb_target_group" "Wildcard-Prod-apac-f4f-com" {
     timeout = 5
     healthy_threshold = 6
     unhealthy_threshold = 2
-    matcher = "200"
+    matcher = "301"
   }
   tags = {
     Name = "Prod"
   }
+}
 
+#####Terrafor Imported Target group -2#########
+resource "aws_lb_target_group" "service-desk" {
+  name = "service-desk"
+  target_type = "instance"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.existing_vpc.id
+    health_check {
+    path = "/health"
+    interval = 35
+    timeout = 5
+    healthy_threshold = 6
+    unhealthy_threshold = 2
+    matcher = "301"
+  }
+  tags = {
+    Name = "Prod-2"
+  }
+}
+
+#####Terrafor Imported Target group -3#########
+resource "aws_lb_target_group" "barclays" {
+  name = "barclays"
+  target_type = "instance"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.existing_vpc.id
+    health_check {
+    path = "/health"
+    interval = 35
+    timeout = 5
+    healthy_threshold = 6
+    unhealthy_threshold = 2
+    matcher = "301"
+  }
+  tags = {
+    Name = "Prod-3"
+  }
 }
 
 ####Terraform Imported ALB######
@@ -73,15 +111,14 @@ resource "aws_lb" "food-lb" {
     data.aws_subnet.reuse-2.id,
     #"subnet-05270ba135519444e"
   ]
-
     tags = {
     Name = "Prod-lb"
   }
 
 }
 
-#####ALB Listener Groups######
-resource "aws_lb_listener" "food-lb" {
+#####ALB Listener Group of HTTP######
+resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.food-lb.arn
   port              = 80
   protocol          = "HTTP"
@@ -92,9 +129,10 @@ resource "aws_lb_listener" "food-lb" {
 }
   }
 
-resource "aws_alb_listener" "food-lb" {
+#####ALB Listener Group of HTTPS######
+resource "aws_alb_listener" "https" {
   load_balancer_arn = aws_lb.food-lb.arn
-  port = "443"
+  port = 443
   protocol = "HTTPS"
   ssl_policy = "ELBSecurityPolicy-2016-08"
   certificate_arn = "arn:aws:acm:ap-southeast-2:036557561906:certificate/d1167f6d-1bde-4417-a0c0-09fe2785ff54"
@@ -102,6 +140,37 @@ resource "aws_alb_listener" "food-lb" {
   default_action {
     type = "forward"
     target_group_arn = aws_lb_target_group.Wildcard-Prod-apac-f4f-com.arn
+
+  }
+}
+
+#####ALB Listener Group of HTTPS RULE-2######
+resource "aws_lb_listener_rule" "HTTPS_Rule_2" {
+  listener_arn = aws_alb_listener.https.arn
+  priority     = 1
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.service-desk.arn
+  }
+  condition {
+    host_header {
+      values = ["service-desk.*"]
+    }
+  }
+}
+
+#####ALB Listener Group of HTTPS RULE-3######
+resource "aws_lb_listener_rule" "HTTPS_Rule_3" {
+  listener_arn = aws_alb_listener.https.arn
+  priority     = 2
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.barclays.arn
+  }
+  condition {
+    host_header {
+      values = ["barclays.*"]
+    }
   }
 }
 
@@ -110,9 +179,23 @@ data "aws_instance" "new_server" {
   instance_id = "i-050bacb3b916583cd"
 }
 
-###Attaching the Instance to Target Group######
-resource "aws_lb_target_group_attachment" "example" {
+###Attaching the Instance to Target Group-1######
+resource "aws_lb_target_group_attachment" "instance-1" {
   target_group_arn = aws_lb_target_group.Wildcard-Prod-apac-f4f-com.arn
+  target_id        = "i-050bacb3b916583cd"
+  port             = 80
+}
+
+###Attaching the Instance to Target Group-2######
+resource "aws_lb_target_group_attachment" "instance-2" {
+  target_group_arn = aws_lb_target_group.service-desk.arn
+  target_id        = "i-050bacb3b916583cd"
+  port             = 80
+}
+
+###Attaching the Instance to Target Group-3######
+resource "aws_lb_target_group_attachment" "instance-3" {
+  target_group_arn = aws_lb_target_group.barclays.arn
   target_id        = "i-050bacb3b916583cd"
   port             = 80
 }
